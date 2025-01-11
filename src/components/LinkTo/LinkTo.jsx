043cb18db864913a80d64255d9607c39f9d4786d
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-01-11 09:04:34 +0300

    LinkTo fix

--- src/components/LinkTo/LinkTo.jsx
+++ src/components/LinkTo/LinkTo.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'; // Импортируем Link из react-router-dom
 import '../../styles/components/LinkTo.scss';
 import LinkArrowIcon from '../SVGIcons/LinkArrowIcon';
 
 const LinkTo = ({ href, text, variant = 'default', className = '', iconColor = '#48AE5A' }) => {
   return (
-    <a href={href} className={`aam_link-to aam_link-to--${variant} ${className}`}>
+    <Link to={href} className={`aam_link-to aam_link-to--${variant} ${className}`}>
       <span className="aam_link-to__text">{text}</span>
       <LinkArrowIcon fillColor={iconColor} className="aam_link-to__icon" />
-    </a>
+    </Link>
   );
 };
 
